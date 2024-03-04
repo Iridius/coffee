32b6272180bb82d3288eae8b0ce2efe642e0bfbe
Melkov Denis <d.a.melkov@gmail.com>
2024-03-04 08:19:40 +0300

    initial commit

---
+++ src/main/java/ru/sovcombank/user/UserApplication.java
@@ -0,0 +1,10 @@
+package ru.sovcombank.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class UserApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+}
